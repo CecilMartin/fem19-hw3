6a08e0f88a818f7d82769f2c45eef4cf9cc43f54
Zhe Chen <zc1291@cims.nyu.edu>
2020-01-03 01:26:51 +0800

    Hw4Ex3

--- problem2/fig_plot.m
+++ problem2/fig_plot.m
@@ -8,3 +8,29 @@ xlabel('1/h')
 ylabel('Error in L_2')
 legend show
 legend('Location','best')
+
+%%
+
+figure; box on; hold on;
+error_x = error(:,1);
+plot(error_x,error(:,2),'-o','LineWidth',1.5,'DisplayName','Prior Error')
+set(gca,'XScale','log','YScale','log')
+order = log(error(end,2)/error(end-1,2))/log(error_x(end)/error_x(end-1))
+plot(error_x,error(end,2)*(error_x/error_x(end)).^-0.5,'--','DisplayName','1/2 order')
+xlabel('# of elements')
+ylabel('Error in L_2')
+legend show
+legend('Location','best')
+
+%%
+
+figure; box on; hold on;
+set(gca,'XScale','log','YScale','log')
+for i=1:length(indicator_rec)
+    plot(indicator_rec{i},'-o','LineWidth',1.5,'DisplayName','Posteior Error')
+end
+% xlabel('# of elements')
+% ylabel('Error in each')
+title('Posterior Error in each Element, sorted descend')
+% legend show
+% legend('Location','best')
